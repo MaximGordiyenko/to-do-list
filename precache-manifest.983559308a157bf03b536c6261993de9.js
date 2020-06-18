1e4ffffc276dc441947ba4da81447920ffa8fbf7
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2020-06-18 13:49:17 +0300

    Updates

---
+++ precache-manifest.983559308a157bf03b536c6261993de9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6682c284f4fc5d3dd4e",
+    "url": "/to-do-list/static/css/main.ed751897.chunk.css"
+  },
+  {
+    "revision": "d6682c284f4fc5d3dd4e",
+    "url": "/to-do-list/static/js/main.edbe7cbb.chunk.js"
+  },
+  {
+    "revision": "15bb3101340a9b514097",
+    "url": "/to-do-list/static/js/runtime~main.9900d293.js"
+  },
+  {
+    "revision": "e84cceccc2841b85d993",
+    "url": "/to-do-list/static/js/2.f4426b14.chunk.js"
+  },
+  {
+    "revision": "ac0ab1de0a522a3d8ec8ea24c84635cd",
+    "url": "/to-do-list/index.html"
+  }
+];
